97e90efbc10852aa6e9e4e8ef56ca4bc84e4d537
Hina Nakahira <soleil.colza@icloud.com>
2024-04-07 16:02:22 +0900

    Add lint checks for Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    lintChecks(libs.compose.lint.checks)
 }
