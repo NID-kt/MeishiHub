9efa34df1765daee43b06e73c14b02e4be7d7c2a
Hina Nakahira <soleil.colza@icloud.com>
2024-04-08 15:26:45 +0900

    Add ScreenShotTest for UI testing

---
+++ app/src/test/java/com/nid/geek_april_android_app/ScreenShotTest.kt
@@ -0,0 +1,35 @@
+package com.nid.geek_april_android_app
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.captureRoboImage
+import com.nid.geek_april_android_app
+import com.nid.geek_april_android_app.ui.theme.Geek_april_android_appTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@RunWith(RobolectricTestRunner::class)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel7Pro)
+class ScreenShotTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun cardsScreenTest() {
+        composeRule.setContent {
+            Geek_april_android_appTheme {
+                Greeting("Android")
+            }
+        }
+        composeRule
+            .onRoot()
+            .assertExists()
+            .captureRoboImage()
+    }
+}
